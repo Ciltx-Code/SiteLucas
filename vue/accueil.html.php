673c7a01ffaac06bce0cbc7f84d44fe5ae00e63e
Lucas <53273785+Ciltx-Code@users.noreply.github.com>
2021-06-08 14:50:55 +0200

    Delete vue directory

--- vue/accueil.html.php
+++
@@ -1,4 +0,0 @@
-	<div class="accueil" id="accueil">
-		<h1>Accueil</h1>
-		<p>« J’ai raté 9000 tirs dans ma carrière. J’ai perdu presque 300 matchs. 26 fois, on m’a fait confiance pour prendre le tir de la victoire et j’ai raté.  J’ai échoué encore et encore et encore dans ma vie. Et c’est pourquoi j’ai réussi. » - Michael Jordan</p>
-	</div>
--- vue/pied.html.php
+++
@@ -1,4 +0,0 @@
-        <div class="pied" id="pied">
-            <p>® Créé par DELOBELLE Théo et GARLASCHI Lucas</p>
-        </div>
-    </div>
